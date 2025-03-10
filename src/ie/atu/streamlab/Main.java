5e88fdd312db7753965417b31b5aa4ac7e9b8047
Daniel Cregg <danielcregg@users.noreply.github.com>
2025-03-10 13:38:08 +0000

    Implemented DIY Task 2: Basic Stream Operations

--- src/ie/atu/streamlab/Main.java
+++ src/ie/atu/streamlab/Main.java
@@ -1,7 +1,17 @@
 package ie.atu.streamlab;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        
+        // DIY Task 2: Basic Stream Operations
+        System.out.println("\nDIY Task 2: Basic Stream Operations");
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        numbers.stream()
+               .filter(n -> n % 2 != 0) // Filter out even numbers
+               .forEach(n -> System.out.print(n + " ")); // Print the remaining numbers
+        System.out.println();
     }
 }
